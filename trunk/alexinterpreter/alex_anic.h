aeac486aecaf748ee88a1518f29d7f2a38173b68
lvzixun@gmail.com <lvzixun@gmail.com@d8813489-4ede-5ca7-a114-1bc83e9fe7c2>
2011-11-24 17:02:37 +0000

    添加了对dll注册的接口函数， 同时添加了using 关键字，来自定义引用外部dll，注册给alex脚本使用。

---
+++ trunk/alexinterpreter/alex_anic.h
@@ -0,0 +1,42 @@
+#ifndef _ALEX_ANIC_H_
+#define _ALEX_ANIC_H_
+
+#include "alex_conf.h"
+#include "alex_vm.h"
+		
+
+typedef void (*FUNC_REG_FUNC)(char*, void*);
+
+typedef ALEX_NUMBER (*FUNC_POP_NUMBER)(void*);
+typedef int (*FUNC_PUSH_NUMBER)(void*, ALEX_NUMBER);
+
+typedef ALEX_STRING (*FUNC_POP_STRING)(void*);
+typedef int (*FUNC_PUSH_STRING)(void*, char*);
+
+typedef alex_al* (*FUNC_POP_AL)(void*);
+typedef int	(*FUNC_PUSH_AL)(void*, r_value);
+
+typedef void* (*FUNC_POP_PTR)(void*);
+typedef int  (*FUNC_PUSH_PTR)(void*, void*);
+
+typedef r_value (*FUNC_POP_FUNC)(void*);
+
+typedef struct _ani
+{
+	FUNC_POP_NUMBER _ani_pop_number;
+	FUNC_PUSH_NUMBER _ani_push_number;
+	
+	FUNC_POP_STRING _ani_pop_string;
+	FUNC_PUSH_STRING _ani_push_string;
+	
+	FUNC_POP_AL	_ani_pop_al;
+	FUNC_PUSH_AL _ani_push_al;
+	
+	FUNC_POP_PTR  _ani_pop_ptr;
+	FUNC_PUSH_PTR _ani_push_ptr;
+	
+	FUNC_POP_FUNC  _ani_pop_func;
+	FUNC_REG_FUNC  _ani_reg_func;
+}ani;
+
+#endif
